596efc222243824914d57116bb70dc26d5f81ce1
shouryasarkar713 <shourya.sarkar@research.iiit.ac.in>
2026-07-18 14:57:08 +0530

    Sync CORS fixes and dynamic download helper api url

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origin_regex="https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
